be77a1e306280d52d960b4ae34b2a20312f1e1d2
ragesalmon <ragesalmon@gmail.com>
2013-12-07 18:06:33 -0500

    Successfully, although very inefficiently, created an array of open windows

--- Tilde++/main.cpp
+++ Tilde++/main.cpp
@@ -1,7 +1,22 @@
 #include "main.h"
-
+#include "Window.h"
+#include <iostream>
 int main()
 {
+	EnumWindows(sendWindowsToTempWindowArray, NULL);
+	WindowArray.resize(numWindows);
+	for(int i = 0; i < numWindows; i++){
+		std::cout << "Window HWND: " << tempWindowArray.at(i) << "\n";
+		std::cout << "Is iconic: " << IsIconic(tempWindowArray.at(i)) << "\n";
+		std::cout << "Is visible: " << IsWindowVisible(tempWindowArray.at(i)) << "\n";
+		std::cout << "Is enabled: " << IsWindowEnabled(tempWindowArray.at(i)) << "\n";
+		if(IsWindowVisible(tempWindowArray.at(i)))
+		{
+			sendWindowsToWindowArray(tempWindowArray.at(i));
+			std::cout << "!!!!!!!!!!!!!!Window sent to array!!!!!!!!!!!!" << "\n";
+		}
+	}
 	LoadLibrary(L"Ship.dll");
+	std::cin.get();
 	return 0;
 }
